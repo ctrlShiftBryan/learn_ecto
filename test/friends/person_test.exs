d4ba0c15788932120c00049ea9688a7db5879df0
Bryan Arendt <barendt@covermymeds.com>
2018-02-02 23:10:54 -0500

    tests no reset

--- test/friends/person_test.exs
+++ test/friends/person_test.exs
@@ -1,7 +1,16 @@
 defmodule PersonTest do
   use ExUnit.Case
+
   test "Can insert a person" do
     person = %Friends.Person{}
     assert {:ok, _} = LearnEcto.Repo.insert(person)
+  end
+
+  test "can validate a person" do
+    person = %Friends.Person{}
+    changeset = Friends.Person.changeset(person, %{})
+
+    assert {:error, %{action: :insert, errors: [first_name: _, last_name: _], valid?: false}} =
+             LearnEcto.Repo.insert(changeset)
   end
 end
